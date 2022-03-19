52804c354d550c970b6a5fbd53cd473c8213a327
Artur <99585873+SinArtIT@users.noreply.github.com>
2022-03-20 00:16:24 +0700

    v1.1

--- app/gallery.js
+++ app/gallery.js
@@ -1,15 +1,16 @@
 let leftArrow = document.querySelector('.left-arrow');
 let rightArrow = document.querySelector('.right-arrow');
 let eventBlocks = document.querySelector('.event-blocks');
-
+let eventsBlock = document.querySelectorAll('.events-block');
+console.log(eventsBlock[0]);
 let left = 0;
 
 leftArrow.onclick = () => {
-  left -= 380;
+  left -= eventsBlock[0].offsetWidth + 30;
   eventBlocks.style.left = `${left}px`;
 };
 
 rightArrow.onclick = () => {
-  left += 380;
+  left += eventsBlock[0].offsetWidth + 30;
   eventBlocks.style.left = `${left}px`;
 }
